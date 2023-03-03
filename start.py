6d229e67e35b78440cf9287f20cf4ea94f23fe2c
sonyang022 <sonyang022@student.wethinkcode.co.za>
2023-03-03 13:30:44 +0200

    moving rectangle

--- start.py
+++ start.py
@@ -2,13 +2,14 @@ import pygame
 import sys
 import random
 def draw_rect(x,y):
-    rectangle = pygame.Rect(0,0,10,10)
+    rectangle = pygame.Rect(x,y,10,10)
     pygame.draw.rect(display_window, green, rectangle)
     pygame.display.flip()
 
 pygame.init()
 green = (0,255,0)
 display_window = pygame.display.set_mode((800,400))
+clock = pygame.time.Clock()
 # print(pygame.K_UP)
 x = 0
 y = 0
@@ -17,5 +18,14 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    draw_rect()
+    draw_rect(x,y)
+    
+    x += 1
+    # if pygame.key.get_pressed()[pygame.K_DOWN] :
+    #     y += 1
+    
+
+    pygame.display.update()
+    clock.tick(60)
+   
     
